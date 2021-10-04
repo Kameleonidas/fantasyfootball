83167ece798f56b281deabcf3b9702754cab0d40
Kameleonidas <kiepura.kam@gmai.com>
2021-10-04 23:22:30 +0200

    Initial commit

---
+++ src/test/java/com/pl/fantasyfootball/FantasyfootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.fantasyfootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyfootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
